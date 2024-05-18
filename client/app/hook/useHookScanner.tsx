d9bd58b05a8a015bcf2072cd4918606145422e03
Alexander Olier <aolierguerrero@gmail.com>
2024-05-17 23:47:07 -0500

    feat: :art: result modal & skeleton

--- client/app/hook/useHookScanner.tsx
+++ client/app/hook/useHookScanner.tsx
@@ -1,13 +1,22 @@
 import { useState } from "react";
+import UseFetchPost from "./UseFetchPost";
+import { Response } from "~/domain/Response";
 
-export default function useHookScanner() {
+type Props = {
+  url: string;
+};
+export default function useHookScanner({ url }: Props) {
   const [modal, setModal] = useState<boolean>(false);
+  const { fetchPost, response } = UseFetchPost<Response, any>();
 
   //add e for event the form
   function handleSubmit(e: React.FormEvent<HTMLFormElement>) {
-    e.preventDefault(); // Prevent default form submission behavior
-    //active modal
+    e.preventDefault();
     setModal(true);
+    console.log(url);
+    fetchPost("http://ec2-3-133-85-14.us-east-2.compute.amazonaws.com:8090/", {
+      web: url,
+    });
   }
-  return { handleSubmit, setModal, modal };
+  return { handleSubmit, setModal, modal, response };
 }
